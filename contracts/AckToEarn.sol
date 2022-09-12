ed97de85e218e1b26b2fdb4b0d4bbd02ab956ce1
Faybian Byrd <fbyrd@datto.com>
2022-09-12 13:28:37 -0400

    Add ability to reclaim bids

--- contracts/AckToEarn.sol
+++ contracts/AckToEarn.sol
@@ -2,22 +2,35 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract AckToEarn is Ownable {
+contract AckToEarn is Ownable, ReentrancyGuard {
+    using Counters for Counters.Counter;
+
     event NewBid(address indexed from, address indexed to, uint amount);
+    event ReclaimBid(address indexed bidder, uint amount);
+
+    Counters.Counter private bidIds;
 
     struct Bid {
+        uint id;
         address bidder;
+        address recipient;
         uint amount;
-        string message;
         uint timestamp;
+        string message;
         bool claimed;
+        bool exists;
     }
 
-    mapping(address => Bid[]) private bidderBids;
-    mapping(address => Bid[]) private recipientBids;
-    mapping(address => uint) private recipientUnclaimedBalances;
+    mapping(address => mapping(uint => Bid)) private bidderBids;
+    mapping(address => mapping(uint => Bid)) private recipientBids;
+
+    Bid[] public bids;
+
+    uint constant bidExpiryThreshold = 7 days;
 
     /**
     * @notice Send a message and bid amount to a recipient
@@ -27,34 +40,85 @@ contract AckToEarn is Ownable {
 
         // The recipient gets 90% of the bid amount
         uint recipientAmount = (msg.value * 90) / 100;
-
-        // Adds the new bid to the list of bids for this bidder
-        bidderBids[msg.sender].push(Bid({
+        uint newBidId = bidIds.current();
+        Bid memory bid = Bid({
+            id: newBidId,
             bidder: msg.sender,
+            recipient: recipient,
             amount: msg.value,
             message: message,
             timestamp: block.timestamp,
-            claimed: false
-        }));
+            claimed: false,
+            exists: true
+        });
 
-        // Adds the new bid to the list of bids for this recipient
-        recipientBids[recipient].push(Bid({
-            bidder: msg.sender,
-            amount: msg.value,
-            message: message,
-            timestamp: block.timestamp,
-            claimed: false
-        }));
+        bids.push(bid);
 
-        recipientUnclaimedBalances[recipient] += recipientAmount;
+        bidderBids[msg.sender][newBidId] = bid;
+        recipientBids[recipient][newBidId] = bid;
 
         emit NewBid(msg.sender, recipient, msg.value);
     }
 
     /**
-    * @notice Get the unclaimed balance for an account
+    * @notice Returns all bids that have ever been made on the platform
     */
-    function getRecipientUnclaimedBalance(address recipient) public view returns(uint) {
-        return recipientUnclaimedBalances[recipient];
+    function getBids() external view returns(Bid[] memory) {
+        return bids;
+    }
+
+    /**
+    * @notice Reclaims a specific set of expired bids that the bidder has made.
+    */
+    function reclaimBids(uint[] memory bidIdsToReclaim) external nonReentrant {
+        uint totalReclaimAmount = 0;
+
+        for (uint i = 0; i < bidIdsToReclaim.length; i++)
+        {
+            uint bidId = bidIdsToReclaim[i];
+            Bid memory bid = bidderBids[msg.sender][bidId];
+
+            if (!bid.exists) {
+                continue;
+            }
+            // If the recipient has already claimed the bid, don't allow the balance to be reclaimed
+            if (bid.claimed) {
+                continue;
+            }
+            // If the bid has not expired yet then don't allow the balance to be reclaimed
+            if (!isBidExpired(bidId, msg.sender)) {
+                continue;
+            }
+
+            uint bidReclaimAmount = (bid.amount * 90) / 100;
+            totalReclaimAmount += bidReclaimAmount;
+            bidderBids[msg.sender][bidId].claimed = true;
+        }
+
+        require(totalReclaimAmount > 0, "No ether to reclaim.");
+
+        (bool success, ) = (msg.sender).call{value: totalReclaimAmount}("");
+
+        require(success, "Failed to send ether.");
+
+        emit ReclaimBid(msg.sender, totalReclaimAmount);
+    }
+
+    /**
+    * @notice Check if bid is expired
+    * @dev Returns true if the bid cannot be found
+    */
+    function isBidExpired(uint bidId, address account) public view returns(bool) {
+        bool isBidderAccount = bidderBids[account][bidId].exists == true;
+        bool isRecipientAccount = recipientBids[account][bidId].exists == true;
+
+        if (isBidderAccount) {
+            return (block.timestamp >= (bidderBids[account][bidId].timestamp + bidExpiryThreshold));
+        }
+        if (isRecipientAccount) {
+            return (block.timestamp >= (recipientBids[account][bidId].timestamp + bidExpiryThreshold));
+        }
+
+        return true;
     }
 }
--- test/AckToEarn.js
+++ test/AckToEarn.js
@@ -1,6 +1,9 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
+// Seven days
+const bidExpiryThreshold = 7 * 24 * 60 * 60;
+
 describe("AckToEarn", function () {
   async function deployAckToEarnFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
@@ -18,27 +21,69 @@ describe("AckToEarn", function () {
     });
   });
 
-  describe("Recipient", function () {
-    it("Should be able to see unclaimed balance", async function () {
-      const { ackToEarn, otherAccount } = await loadFixture(deployAckToEarnFixture);
+  describe("Bidder", function () {
+    it("Should be able to reclaim bid amounts.", async function () {
+      const { ackToEarn, owner, otherAccount } = await loadFixture(deployAckToEarnFixture);
       const bidAmount = ethers.utils.parseEther("1");
-      const recipientAmount = (bidAmount * 90) / 100;
+      const reclaimAmount = (bidAmount * 90) / 100;
 
       await ackToEarn.sendBid("Some message", otherAccount.address, {value: bidAmount});
 
-      expect(await ackToEarn.getRecipientUnclaimedBalance(otherAccount.address))
-          .to.equal(recipientAmount.toString());
+      // Increase the EVM time by the bid expiry threshold (7 days)
+      await ethers.provider.send('evm_increaseTime', [bidExpiryThreshold]);
+      await ethers.provider.send('evm_mine');
+
+      const bids = await ackToEarn.getBids();
+      let bidderBidIds = [];
+
+      bids.forEach(bid => {
+        if (bid.bidder === owner.address) {
+          bidderBidIds.push(ethers.BigNumber.from(bid.id).toNumber());
+        }
+      });
+
+      // Ensure the bidder's wallet balance is updated with the correct amount of ether
+      expect(await ackToEarn.reclaimBids(bidderBidIds)).to.changeEtherBalance(owner, reclaimAmount.toString());
+
+      // Ensure the bidder cannot reclaim the same bid multiple times
+      await expect(ackToEarn.reclaimBids(bidderBidIds)).to.be.revertedWith("No ether to reclaim.");
+    });
+  });
+
+  describe("Events", function () {
+    it("Should emit an event on new bid", async function () {
+      const { ackToEarn, owner, otherAccount } = await loadFixture(deployAckToEarnFixture);
+      const bidAmount = ethers.utils.parseEther("1");
+
+      expect(await ackToEarn.sendBid("Some message", otherAccount.address, {value: bidAmount}))
+          .to.emit(ackToEarn, "NewBid")
+          .withArgs(owner.address, otherAccount, bidAmount);
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { ackToEarn, owner, otherAccount } = await loadFixture(deployAckToEarnFixture);
-        const bidAmount = ethers.utils.parseEther("1");
 
-        expect(await ackToEarn.sendBid("Some message", otherAccount.address, {value: bidAmount}))
-            .to.emit(ackToEarn, "NewBid")
-            .withArgs(owner.address, otherAccount, bidAmount);
+    it("Should emit an event when bids are reclaimed", async function () {
+      const { ackToEarn, owner, otherAccount } = await loadFixture(deployAckToEarnFixture);
+      const bidAmount = ethers.utils.parseEther("1");
+      const reclaimAmount = (bidAmount * 90) / 100;
+
+      await ackToEarn.sendBid("Some message", otherAccount.address, {value: bidAmount});
+
+      // Increase the EVM time by the bid expiry threshold (7 days)
+      await ethers.provider.send('evm_increaseTime', [bidExpiryThreshold]);
+      await ethers.provider.send('evm_mine');
+
+      const bids = await ackToEarn.getBids();
+      let bidderBidIds = [];
+
+      bids.forEach(bid => {
+        if (bid.bidder === owner.address) {
+          bidderBidIds.push(ethers.BigNumber.from(bid.id).toNumber());
+        }
       });
+
+      expect(await ackToEarn.reclaimBids(bidderBidIds))
+          .to.emit(ackToEarn, "ReclaimBid")
+          .withArgs(owner.address, reclaimAmount.toString());
     });
   });
 });
