ca1a2eef560515b347a3e81b14818e002cb2f29b
pardhaponugoti <pardhapon@gmail.com>
2022-09-13 09:18:31 -0400

    Add connect wallet button

--- src/App.js
+++ src/App.js
@@ -1,65 +1,79 @@
-import React, { useEffect, useState } from "react";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import {
-  Routes,
-  Route,
-  Link,
-  useLocation,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import { ethers } from "ethers";
-import detectEthereumProvider from '@metamask/detect-provider'
+import detectEthereumProvider from "@metamask/detect-provider";
 
-import SendMessagePage from './components/SendMessagePage';
-import YourMessagesPage from './components/YourMessagesPage';
-import { 
-  PAGE_ROUTE_SEND_MESSAGE, 
-  PAGE_ROUTE_YOUR_MESSAGES 
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+import SendMessagePage from "./components/SendMessagePage";
+import YourMessagesPage from "./components/YourMessagesPage";
+
+import {
+  PAGE_ROUTE_SEND_MESSAGE,
+  PAGE_ROUTE_YOUR_MESSAGES,
 } from "./constants/Routing";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [address, setAddress] = useState("");
+  const [walletAddress, setWalletAddress] = useState(null);
 
-  useEffect(() => {
-    const getWallet = async () => {
-      const ethereum = await detectEthereumProvider();
-      const provider = new ethers.providers.Web3Provider(ethereum, "any");
-      await provider.send("eth_requestAccounts", []);
-      const address = await provider.getSigner().getAddress();
-      setAddress(address);
-    };
-
-    getWallet();
-  });
+  const getWallet = async () => {
+    const ethereum = await detectEthereumProvider();
+    const provider = new ethers.providers.Web3Provider(ethereum, "any");
+    await provider.send("eth_requestAccounts", []);
+    const address = await provider.getSigner().getAddress();
+    setWalletAddress(address);
+  };
 
   let selectedTab = 0;
   const location = useLocation();
   if (location.pathname.includes(PAGE_ROUTE_SEND_MESSAGE)) {
     selectedTab = 1;
-  } else if ((location.pathname.includes(PAGE_ROUTE_YOUR_MESSAGES))) {
-    selectedTab = 2;  
+  } else if (location.pathname.includes(PAGE_ROUTE_YOUR_MESSAGES)) {
+    selectedTab = 2;
   }
 
   return (
     <div className="App">
       <div>
-        {address ? (
-          <p>
-            <strong>Wallet address:</strong> {address}
-          </p>
-        ) : (
-          <p>Fetching wallet..</p>
-        )}
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, textAlign: "left" }}
+            >
+              Ack To Earn
+            </Typography>
+            {walletAddress ? (
+              <Typography
+                variant="p"
+                component="div"
+                sx={{ flexGrow: 1, textAlign: "right" }}
+              >
+                {walletAddress}
+              </Typography>
+            ) : (
+              <Button color="inherit" onClick={getWallet}>
+                Connect Wallet
+              </Button>
+            )}
+          </Toolbar>
+        </AppBar>
       </div>
       <Tabs value={selectedTab} aria-label="basic tabs example">
-        <Link to={"/"} style={{textDecoration: "none"}}>
+        <Link to={"/"} style={{ textDecoration: "none" }}>
           <Tab label="How it works" />
         </Link>
-        <Link to={PAGE_ROUTE_SEND_MESSAGE} style={{textDecoration: "none"}}>
+        <Link to={PAGE_ROUTE_SEND_MESSAGE} style={{ textDecoration: "none" }}>
           <Tab label="Send Message" />
         </Link>
-        <Link to={PAGE_ROUTE_YOUR_MESSAGES} style={{textDecoration: "none"}}>
+        <Link to={PAGE_ROUTE_YOUR_MESSAGES} style={{ textDecoration: "none" }}>
           <Tab label="Your Messages" />
         </Link>
       </Tabs>
@@ -67,7 +81,10 @@ function App() {
         <Route path="/">
           <Route index element={<div>Welcome to AckToEarn!</div>} />
           <Route path={PAGE_ROUTE_SEND_MESSAGE} element={<SendMessagePage />} />
-          <Route path={PAGE_ROUTE_YOUR_MESSAGES} element={<YourMessagesPage />} />
+          <Route
+            path={PAGE_ROUTE_YOUR_MESSAGES}
+            element={<YourMessagesPage />}
+          />
         </Route>
       </Routes>
     </div>
