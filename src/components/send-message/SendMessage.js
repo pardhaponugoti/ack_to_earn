58a9e8a7e06600d7e48c2fa7aaabe5b3039bf3fe
electrone901 <carbajalluis@rocketmail.com>
2022-09-13 11:35:35 -0400

    add send-message page

--- src/components/send-message/SendMessage.js
+++ src/components/send-message/SendMessage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./SendMessage.css";
+import question from "../../images/question.png";
 function SendMessage() {
   const [recipientWallet, setRecipientWallet] = useState("");
   const [message, setMessage] = useState("");
@@ -38,6 +39,7 @@ function SendMessage() {
 
           <label className="block">
             {/* <span id="onhoverresponse">TEST</span> */}
+            {/*
             <div className="flex">
               <span className="text-gray-700">Response address </span>
               <svg
@@ -54,6 +56,18 @@ function SendMessage() {
                   d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
                 />
               </svg>
+            </div> */}
+
+            {/* OLD  */}
+
+            <div className="img__wrap flex">
+              <span className="text-gray-700">Response address </span>
+              <div>
+                <img className="img__img" src={question} alt="question" />
+                <p className="img__description">
+                  How recipient can get in touch with you.
+                </p>
+              </div>
             </div>
 
             <input
