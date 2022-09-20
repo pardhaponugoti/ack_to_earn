bcdf1f710534af9e57721d302c09ac94fa544f1b
pardhaponugoti <pardhapon@gmail.com>
2022-09-20 11:23:38 -0400

    hotfix: address merge conflict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Routes, Route, Link, useLocation } from "react-router-dom";
 import { ethers } from "ethers";
 import detectEthereumProvider from "@metamask/detect-provider";
-import { getBalance } from "./utils/Contract";
 
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
@@ -34,11 +33,9 @@ function App() {
     await provider.send("eth_requestAccounts", []);
     const address = await provider.getSigner().getAddress();
     const ensName = await provider.lookupAddress(address);
-    const balanceEth = await getBalance(provider, address);
 
     setWalletProvider(provider);
     setWalletAddress(ensName ?? address);
-    setBalance(balanceEth);
   };
 
   let selectedTab = 0;
--- src/components/YourMessagesPage.js
+++ src/components/YourMessagesPage.js
@@ -51,6 +51,7 @@ const YourMessagesPage = (props) => {
             ...message,
           };
         });
+
       setAllMessages(yourMessages);
     };
 
