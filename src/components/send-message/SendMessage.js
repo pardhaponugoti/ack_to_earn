4dc4568938bf0bfa0584459e703060bff935b86e
pardhaponugoti <pardhapon@gmail.com>
2022-09-20 13:51:08 -0400

    Add file back

--- src/components/send-message/SendMessage.js
+++ src/components/send-message/SendMessage.js
@@ -148,6 +148,18 @@ function SendMessage(props) {
             />
           </label>
 
+          <label className="block">
+            <span className="text-gray-700">Attach a file (optional)</span>
+            <input
+              type="file"
+              className="shadow-sm bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-ligh"
+              onChange={(e) => {
+                setAttachedFile(e.target.files[0]);
+              }}
+              disabled={isLoading}
+            />
+          </label>
+
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
