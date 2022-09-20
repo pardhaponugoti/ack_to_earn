ce9eae9fda206da3f49b0758c2fbba861d4f440c
pardhaponugoti <pardhapon@gmail.com>
2022-09-20 12:57:29 -0400

    update UI to use new contract

--- src/components/send-message/SendMessage.js
+++ src/components/send-message/SendMessage.js
@@ -129,7 +129,7 @@ function SendMessage(props) {
           </label>
 
           <label className="block">
-            <span className="text-gray-700">Bid amount</span>
+            <span className="text-gray-700">Bid amount (ETH)</span>
             <input
               type="number"
               className="shadow-sm bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-ligh"
